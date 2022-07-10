c06e0538f617ad7409211a805cb1c236a244bf9a
nikita2956 <nikita.bykov.1996@gmail.com>
2022-07-10 23:57:32 +0300

    Add auth and migrate for record table

---
+++ database/migrations/2022_07_10_205438_create_record_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRecordTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('record', function (Blueprint $table) {
+            $table->id();
+            $table->string("name", 255)->nullable(false);
+            $table->string("email", 150)->unique();
+            $table->string("description", 500);
+            $table->boolean("published")->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('record');
+    }
+}
